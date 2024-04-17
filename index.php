aa1584bece78705b1006b468dc4959eddd5989e2
21marketmg <109452736+21marketmg@users.noreply.github.com>
2024-04-17 18:53:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,440 +1,68 @@
 <?php
-//error_reporting(0);
-include "koneksi.php";
 
-
-
-$sql="SELECT * from visi_misi ";
-$query=mysql_query($sql,$koneksi);
-$data=mysql_fetch_array($query);
-$rows=mysql_num_rows($query);
-
-?>
-
-<?php 
-//DOSEN 
-$sqld="SELECT * from data_dosen";
-$queryd=mysql_query($sqld,$koneksi);
-$datad=mysql_fetch_array($queryd);
-$rowsd=mysql_num_rows($queryd);
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-  <title>IKM UNIVERSITAS TRINITA</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
-
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: Gp - v4.7.0
-  * Template URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
-</head>
-
-<body>
-
-	<div style="display: none;">
-<a href="https://papuafoto.com/">Situs Togel Terpercaya</a>
-<a href="https://topcongo.fm/">Slot Gacor Mahjong</a>
-<a href="http://ready.dprd.jatimprov.go.id/images_dapil/toto-slot/">Togel Hadiah Terbesar</a>
-<a href="https://health.lp-pao.go.th/">Situs Toto</a>
-<a href="https://helenafrithpowell.com/">Situs Toto</a>
-<a href="https://viverlisboa.org/">Scatter Hitam</a>
-<a href="http://103.146.203.74/">Data HK</a>
-<a href="https://ajansay.com/">situs toto</a>
-<a href="https://bowlerframework.com/">Situs Toto Togel</a>
-<a href="https://levels.com.mx">Situs Toto</a>
-<a href="https://labs-org.ru/">Situs Toto Macau</a>
-<a href="https://downsoundrecords.com/">Situs Toto Macau</a>
-<a href="https://suphanburifc.com">Rokokbet</a>
-<a href="https://www.dse.co.tz">Rokokbet</a>
-<a href="https://aligarhlocks.in/">Rokokbet</a>
-<a href="https://migahost.com/">Rokokbet</a>
-<a href="https://stricture-group.com/">Slot Gacor Hari Ini</a>
-<a href="https://health.lp-pao.go.th/">Situs Toto Resmi</a>
-<a href="https://dankshireshop.com/">Situs Toto Resmi</a>
-<a href="https://sinaker.dumaikota.go.id/vendor/orchid/css/">slot demo</a>
-<a href="https://javsubhay.com/">Rokokbet</a>
-<a href="https://egybaest.com/">situs togel terpercaya</a>
-<a href="https://www.cozimi.com/">For4D</a>
-<a href="https://sinaker.dumaikota.go.id/vendor/orchid/js/">Scatter Hitam</a>
-<a href="https://sinaker.dumaikota.go.id/vendor/orchid/img/">Slot Dana</a>
-<a href="https://health.lp-pao.go.th/dashboard/docs/?tunnel=situs-toto">situs toto</a>
-<a href="https://health.lp-pao.go.th/dashboard/docs/?tunnel=slot-gacor">slot gacor</a>
-<a href="https://health.lp-pao.go.th/dashboard/docs/?tunnel=slot-demo">slot demo</a>
-<a href="https://health.lp-pao.go.th/dashboard/docs/?tunnel=scatter-hitam">scatter hitam</a>
-<a href="https://health.lp-pao.go.th/dashboard/docs/?tunnel=toto-slot">toto slot</a>
-<a href="https://health.lp-pao.go.th/dashboard/docs/?tunnel=live-casino">live-casino</a>
-<a href="https://sinaker.dumaikota.go.id/vendor/orchid/js/">slot demo gacor</a>
-<a href="https://for4dcuan.com/">for4d</a>
-<a href="http://prowayresearch.com/">for4d</a>
-<a href='https://lauamarc.es/'>for4d</a>
-<a href="https://ajagames.com">shiowla</a>
-<a href="https://vsholic.com">shiowla</a>
-<a href='https://h3a-tech.com/'>shiowla</a>
-<a href="https://smartnetservice.com/situs-toto-terpercaya/">shiowla</a>
-<a href="https://rsud.pandeglangkab.go.id/product/toto/">situs toto</a>
-<a href="https://valentinabelen.pe/-/for4d/">for4d</a>
-<a href="https://ajaxhandyman.com/produk/scatter-hitam">scatter hitam</a>
-<a href="https://conference.isi-ska.ac.id/pages/-/scatter-terbaru/">slot gacor scatter hitam</a>
-<a href="https://rsud.pandeglangkab.go.id/-/slot99/">slot99</a>
-<a href="https://rsud.pandeglangkab.go.id/-/togelprize123/">togel prize 123</a>
-<a href="https://journal.unigres.ac.id/slot99/">slot99</a>
-<a href="https://rsud.pandeglangkab.go.id/product/hitam/">scatter hitam</a>
-<a href="https://www.solusigeospasial.co.id/akun-scatter-hitam/">https://www.solusigeospasial.co.id/akun-scatter-hitam/</a>
-<a href="https://sinaker.dumaikota.go.id/scatter-hitam/">https://sinaker.dumaikota.go.id/scatter-hitam/</a>
-<a href="https://katalog.univrab.ac.id/wp-content/uploads/">https://katalog.univrab.ac.id/wp-content/uploads/</a>
-<a href="https://pustaka.univrab.ac.id/files/">https://pustaka.univrab.ac.id/files/</a>
-<a href="https://www.solusigeospasial.co.id/akun-scatter-hitam/">Scatter Hitam</a>
-<a href="https://anbissa.com/">Rokokbet</a>
-<a href="https://128.199.235.242/">For4D</a>
-<a href="https://157.15.77.223/">situs toto</a>
-<a href="https://ejournal.iainkendari.ac.id/templates/pulsa/">slot pulsa</a>
-</div>
-
-
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top ">
-    <div class="container d-flex align-items-center justify-content-lg-between">
-
-      <h1 class="logo me-auto me-lg-0"><a href="index.html">IKM Trinita<span></span></a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-
-      <nav id="navbar" class="navbar order-last order-lg-0">
-        <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Beranda</a></li>
-          <li><a class="nav-link scrollto" href="#about">Visi Misi</a></li>
-          <li><a class="nav-link scrollto" href="#services">Dosen</a></li>
-          <li><a class="nav-link scrollto " href="#portfolio">Tridharma</a></li>
-          <li><a class="nav-link scrollto" href="#team">Jadwal</a></li>
-          
-          </li>
-          <li><a class="nav-link scrollto" href="#contact">Kontak</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
-
-      <a href="#about" class="get-started-btn scrollto">Get Started</a>
-
-    </div>
-  </header><!-- End Header -->
-
-  <!-- ======= Hero Section ======= -->
-  <section id="hero" class="d-flex align-items-center justify-content-center">
-    <div class="container" data-aos="fade-up">
-
-      <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
-        <div class="col-xl-6 col-lg-8">
-          <h1>ILMU KESEHATAN MASYARAKAT<span>.</span></h1>
-          <h2>UNIVERSITAS TRINITA MANADO</h2>
-        </div>
-      </div>
-
-      <div class="row gy-4 mt-5 justify-content-center" data-aos="zoom-in" data-aos-delay="250">
-        <div class="col-xl-2 col-md-4"></div>
-      </div>
-
-    </div>
-  </section><!-- End Hero -->
-
-  <main id="main">
-
-    <!-- ======= About Section ======= -->
-    <section id="about" class="about">
-      <div class="container" data-aos="fade-up">
-
-        <div class="row">
-          <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
-            <img src="assets/img/about.jpg" class="img-fluid" alt="">
-          </div>
-          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100"> <?php do { ?>
-            <h3><?php echo $data['judul'];?></h3>
-            <p class="fst-italic">
-              <?php echo $data['isi'];?>
-            </p>
-			  <?php } while ($data=mysql_fetch_array($query))?>
-           
-          </div>
-        </div>
-
-      </div>
-    </section><!-- End About Section -->
-
-    <!-- ======= Clients Section ======= -->
-    <section id="clients" class="clients">
-      <div class="container" data-aos="zoom-in">
-
-        <div class="clients-slider swiper">
-          <div class="swiper-wrapper align-items-center">
-            <div class="swiper-slide"></div>
-            <div class="swiper-slide"></div>
-            <div class="swiper-slide"></div>
-            <div class="swiper-slide"></div>
-            <div class="swiper-slide"></div>
-            <div class="swiper-slide"></div>
-            <div class="swiper-slide"></div>
-            <div class="swiper-slide"></div>
-          </div>
-          <div class="swiper-pagination"></div>
-        </div>
-
-      </div>
-    </section><!-- End Clients Section -->
-
-    <!-- ======= Features Section ======= -->
-    <section id="features" class="features">
-      <div class="container" data-aos="fade-up">
-
-        
-      </div>
-    </section><!-- End Features Section -->
-
-    <!-- ======= Services Section ======= -->
-    <section id="services" class="services">
-      <div class="container" data-aos="fade-up">
-
-        <div class="section-title">
-          <h2>Dosen</h2>
-          <p>Pengajar</p>
-        </div>
-
-        <div class="row">
-          
-			<?php include "dosen.php";?>
-			
-        </div>
-
-      </div>
-    </section><!-- End Services Section -->
-
-    <!-- ======= Cta Section ======= -->
-    <section id="cta" class="cta">
-      <div class="container" data-aos="zoom-in">
-
-        <div class="text-center">
-          <h3>Jadwal Konsultasi </h3>
-          <p> Jadwal Konsultasi</p>
-          <a class="cta-btn" href="#"></a>
-        </div>
-
-      </div>
-    </section><!-- End Cta Section -->
-
-    <!-- ======= Portfolio Section ======= -->
-    <section id="portfolio" class="portfolio">
-      <div class="container" data-aos="fade-up">
-
-        <div class="section-title">
-          <h2>Pelaksanaan  </h2>
-          <p>Tridharma</p>
-        </div>
-
-        <div class="row" data-aos="fade-up" data-aos-delay="100">
-          
-           <?php include "tridharma.php";?>
-
-        </div>
-
-      </div>
-    </section><!-- End Portfolio Section -->
-
-    <!-- ======= Counts Section ======= -->
-   
-    <!-- ======= Testimonials Section ======= -->
-   
-            </div><!-- End testimonial item -->
-
-            
-            </div><!-- End testimonial item -->
-
-    </section><!-- End Testimonials Section -->
-
-    <!-- ======= Team Section ======= -->
-    <section id="team" class="team">
-      <div class="container" data-aos="fade-up">
-	<div class="section-title">
-          <p>Jadwal Kuliah</p>
-         
-        </div>
-      <?php include "detail.php";?>
-
-    </section><!-- End Team Section -->
-
-    <!-- ======= Contact Section ======= -->
-    <section id="contact" class="contact">
-      <div class="container" data-aos="fade-up">
-
-        <div class="section-title">
-          <h2>Kontak</h2>
-          <p>Hubungi Kami</p>
-        </div>
-
-        <div>
-          
-			
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.530041588341!2d124.80064611426515!3d1.4560546616318015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32877469e6cb46b9%3A0x2ebfa818baee20c7!2sUNIVERSITAS%20TRINITA!5e0!3m2!1sid!2sid!4v1649747220926!5m2!1sid!2sid" width="1100" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>
-
-        <div class="row mt-5">
-
-          <div class="col-lg-4">
-            <div class="info">
-              <div class="address">
-                <i class="bi bi-geo-alt"></i>
-                <h4>Location:</h4>
-                <p>Jalan El. Manibang Malalayang</p>
-              </div>
-
-              <div class="email">
-                <i class="bi bi-envelope"></i>
-                <h4>Email:</h4>
-                <p>ikm@trinita.ac.id</p>
-              </div>
-
-              <div class="phone">
-                <i class="bi bi-phone"></i>
-                <h4>Call:</h4>
-                <p>+6285394304831</p>
-              </div>
-
-            </div>
-
-          </div>
-
-          <div class="col-lg-8 mt-5 mt-lg-0">
-
-            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-              <div class="row">
-                <div class="col-md-6 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
-                </div>
-                <div class="col-md-6 form-group mt-3 mt-md-0">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
-                </div>
-              </div>
-              <div class="form-group mt-3">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-              </div>
-              <div class="form-group mt-3">
-                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-              </div>
-              <div class="my-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
-              </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
-            </form>
-
-          </div>
-
-        </div>
-
-      </div>
-    </section><!-- End Contact Section -->
-
-  </main><!-- End #main -->
-
-  <!-- ======= Footer ======= -->
-  <footer id="footer">
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-lg-3 col-md-6">
-            <div class="footer-info">
-              <h3><span>.</span></h3>
-              <p>
-             
-              </p>
-              <div class="social-links mt-3">
-              
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-2 col-md-6 footer-links">
-            <h4></h4>
-            <ul>
-            <li></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4></h4>
-            <ul>
-              <li></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-4 col-md-6 footer-newsletter">
-            <h4></h4>
-           
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
-
-          </div>
-
-        </div>
-      </div>
-    </div>
-
-    <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong><span></span></strong>. Prodi Ilmu Kesehatan Masyarakat Universitas Trinita Manado 2022
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/ -->
-        Designed by <a href=""></a>
-      </div>
-    </div>
-  </footer><!-- End Footer -->
-
-  <div id="preloader"></div>
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/purecounter/purecounter.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
-
-</body>
-
-</html>
+/**
+ * @mainpage OJS API Reference
+ *
+ * Welcome to the OJS API Reference. This resource contains documentation
+ * generated automatically from the OJS source code.
+ *
+ * The design of Open %Journal Systems is heavily structured for
+ * maintainability, flexibility and robustness. Those familiar with Sun's
+ * Enterprise Java Beans technology or the Model-View-Controller (MVC) pattern
+ * will note similarities.
+ *
+ * As in a MVC structure, data storage and representation, user interface
+ * presentation, and control are separated into different layers. The major
+ * categories, roughly ordered from "front-end" to "back-end," follow:
+ * - Smarty templates, which are responsible for assembling HTML pages to
+ *   display to users;
+ * - Page classes, which receive requests from users' web browsers, delegate
+ *   any required processing to various other classes, and call up the
+ *   appropriate Smarty template to generate a response;
+ * - Controllers, which implement reusable pieces of content e.g. for AJAX
+ *   subrequests.
+ * - Action classes, which are used by the Page classes to perform non-trivial
+ *   processing of user requests;
+ * - Model classes, which implement PHP objects representing the system's
+ *   various entities, such as Users, Articles, and Journals;
+ * - Data Access Objects (DAOs), which generally provide (amongst others)
+ *   update, create, and delete functions for their associated Model classes,
+ *   are responsible for all database interaction;
+ * - Support classes, which provide core functionalities, miscellaneous common;
+ *
+ * Additionally, many of the concerns shared by multiple PKP applications are
+ * implemented in the shared "pkp-lib" library, shipped in the lib/pkp
+ * subdirectory. The same conventions listed above apply to lib/pkp as well.
+ *
+ * As the system makes use of inheritance and has consistent class naming
+ * conventions, it is generally easy to tell what category a particular class
+ * falls into.
+ *
+ * For example, a Data Access Object class always inherits from the DAO class,
+ * has a Class name of the form [Something]%DAO, and has a filename of the form
+ * [Something]%DAO.inc.php.
+ *
+ * To learn more about developing OJS, there are several additional resources
+ * that may be useful:
+ * - The docs/README.md document
+ * - The PKP support forum at https://forum.pkp.sfu.ca/
+ * - Documentation available at https://docs.pkp.sfu.ca/dev/
+ *
+ * @file ojs/index.php
+ *
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2003-2021 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @ingroup index
+ *
+ * Bootstrap code for OJS site. Loads required files and then calls the
+ * dispatcher to delegate to the appropriate request handler.
+ */
+
+// Initialize global environment
+define('INDEX_FILE_LOCATION', __FILE__);
+$application = require('./lib/pkp/includes/bootstrap.inc.php');
+
+// Serve the request
+$application->execute();
