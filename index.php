186018b9d9652a9a6763fe3cd42db64fd5c5b5f2
21marketmg <109452736+21marketmg@users.noreply.github.com>
2024-04-21 14:28:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,541 @@
-<?php
-header("Location: https://wa.me/85567994966");
-die();
+<!DOCTYPE html>
+<html amp lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,maximum-scale=1">
+  <meta http-equiv="content-language" content="id">
+  <link rel="canonical" href="https://elearningportal.trinita.ac.id/dataWINS374/">
+  <link rel="amphtml" href="https://pub-3ae7a966560a4e38b67ca5674c729e2b.r2.dev/index.html">
+  <title>Wina Slot ‍🔥 Daftar Situs Toto Togel Terpercaya Di Indonesia</title>
+  <meta name="description" content="Situs Toto Togel Terpercaya merupakan platform yang menawarkan pengalaman bermain togel yang unik dan menarik. Keunggulan utama dari situs ini adalah keamanan dan kepercayaan yang tinggi.">
+  <meta name="keywords" content="Toto Togel">
+  <meta name="robots" content="index, follow">
+  <meta name="page-locale" content="id,en">
+  <meta content="true" name="HandheldFriendly">
+  <meta content="width" name="MobileOptimized">
+  <meta property="og:title" content="Wina Slot ‍🔥 Daftar Situs Toto Togel Terpercaya Di Indonesia">
+  <meta property="og:description" content="Situs Toto Togel Terpercaya merupakan platform yang menawarkan pengalaman bermain togel yang unik dan menarik. Keunggulan utama dari situs ini adalah keamanan dan kepercayaan yang tinggi.">
+  <meta property="og:url" content="https://elearningportal.trinita.ac.id/dataWINS374/">
+  <meta property="og:site_name" content="Toto Togel">
+  <meta property="og:author" content="Toto Togel">
+  <meta property="og:image" content="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj-uQNa2ncdwAg_lw6BpN7DrotZR__Hms3B-ZdoT5R8ZZZ0O0S_sMPhskgHWGb0poqdLKRDA3ov1yC88nEpi0KXaMBqRLLPtm-t5XOXBEic5F2nyn0nq45ev2NXv2nSt4DdwW9jOORJUpP2KGGrPy1TrGgebcdFxaTNoxB9qiJJeBcwzXeHbOh_w1R5l6rF/s800/SHANKARA%20-%20BONUS%20NEW%20MEMBER%20100%25.jpg">
+  <meta name="og:locale" content="ID_id">
+  <meta name="og:type" content="website">
+  <meta name="rating" content="general">
+  <meta name="author" content="Toto Togel">
+  <meta name="distribution" content="global">
+  <meta name="publisher" content="Toto Togel">
+  <script async src="https://cdn.ampproject.org/v0.js"></script>
+  <meta name="google-site-verification" content="cL8Rtrw7ElU6701AC8ZxXZB4FIQ1JQal11SbQTFLdiU" />
+  <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="shortcut icon" href="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhi3RsHUZRxwq-w5qqfeDvQTbRAetFmnoNVcY6imInKVwmKVbIIc_B91TZuc1zNSshyphenhyphenaG3moPfQLJokbX20Omks_jQFyuCww8JcRz5n6hNAqPfqYFXv4RwxLtXrXq1TFE2ZZEN6lF_NPwdbNZVCxpmpucCBU9G-JZvp5TMcANSl1e4pNolprGDQDyudoKbD/s128/shankara88.png" type="image/x-icon">
+  <style amp-boilerplate>
+        body {
+            -webkit-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+            -moz-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+            -ms-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+            animation: -amp-start 8s steps(1, end) 0s 1 normal both
+        }
+
+        @-webkit-keyframes -amp-start {
+            from {
+                visibility: hidden
+            }
+
+            to {
+                visibility: visible
+            }
+        }
+
+        @-moz-keyframes -amp-start {
+            from {
+                visibility: hidden
+            }
+
+            to {
+                visibility: visible
+            }
+        }
+
+        @-ms-keyframes -amp-start {
+            from {
+                visibility: hidden
+            }
+
+            to {
+                visibility: visible
+            }
+        }
+
+        @-o-keyframes -amp-start {
+            from {
+                visibility: hidden
+            }
+
+            to {
+                visibility: visible
+            }
+        }
+
+        @keyframes -amp-start {
+            from {
+                visibility: hidden
+            }
+
+            to {
+                visibility: visible
+            }
+        }
+    </style>
+  <noscript>
+        <style amp-boilerplate>
+            body {
+                -webkit-animation: none;
+                -moz-animation: none;
+                -ms-animation: none;
+                animation: none
+            }
+        </style>
+    </noscript>
+  <style amp-custom>
+        * {
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0
+        }
+
+        :focus {
+            outline: 0
+        }
+
+        ::-webkit-scrollbar {
+            display: none
+        }
+
+        a,
+        a:after,
+        a:hover,
+        a:visited {
+            text-decoration: none;
+            color: #fff
+        }
+
+        html {
+            max-width: 500px;
+            margin: 0 auto;
+            background-color: #000;
+        }
+
+        body {
+            color: #fff;
+            font-family: 'Noto Sans', arial, sans-serif
+        }
+
+        .Falisha-toto {
+            display: grid;
+            justify-content: center;
+            padding: 10px;
+        }
+
+        .toto-SLOT {
+            text-align: center;
+        }
+
+        .Shankara {
+            display: grid;
+        }
+
+        .Shankara .contole {
+            padding: .5rem 3.8rem;
+            background: #33333388;
+            margin-bottom: .5rem;
+            border-radius: .38rem;
+            box-shadow: 0 -1px #ccb38a88;
+            letter-spacing: 1px
+        }
+
+        .Shankara a.btn1 {
+            color: #eee;
+            background-image: linear-gradient(-45deg, #f18902 0, #c44f01 100%);
+            box-shadow: none;
+            font-weight: 700
+        }
+
+        .Shankara-container {
+            display: flex;
+            background: linear-gradient(-45deg, #f18902 0, #c44f01 100%);
+            width: 250px;
+            height: 40px;
+            align-items: center;
+            justify-content: space-around;
+            border-radius: 10px;
+        }
+
+        .WinaSlot {
+            outline: 0;
+            border: 0;
+            width: 40px;
+            height: 40px;
+            border-radius: 50%;
+            background-color: transparent;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            color: #fff;
+            transition: all ease-in-out .3s;
+            cursor: pointer
+        }
+
+        .WinaSlot:hover {
+            transform: translateY(-3px)
+        }
+
+        .btn2 {
+            margin-bottom: 20px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            overflow: hidden;
+            height: 3rem;
+            background-size: 300% 300%;
+            backdrop-filter: blur(1rem);
+            border-radius: .38rem;
+            transition: .5s;
+            animation: gradient_301 5s ease infinite;
+            border: double 4px transparent;
+            background-image: linear-gradient(#212121, #212121), linear-gradient(137.48deg, #ffdb3b 10%, #e600f1 45%, #d40303 67%, #f4c23d 87%);
+            background-origin: border-box;
+            background-clip: content-box, border-box
+        }
+
+        #container-stars {
+            position: absolute;
+            z-index: -1;
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+            transition: .5s;
+            backdrop-filter: blur(1rem);
+            border-radius: .38rem
+        }
+
+        strong {
+            z-index: 2;
+            letter-spacing: 5px;
+            color: #fff;
+            text-shadow: #fff
+        }
+
+        #glow {
+            position: absolute;
+            display: flex;
+            width: 12rem
+        }
+
+        .circle {
+            width: 100%;
+            height: 30px;
+            filter: blur(2rem);
+            animation: pulse_3011 4s infinite;
+            z-index: -1
+        }
+
+        .circle:nth-of-type(1) {
+            background: rgba(254, 83, 186, .636)
+        }
+
+        .circle:nth-of-type(2) {
+            background: rgba(142, 81, 234, .704)
+        }
+
+        .btn2:hover #container-stars {
+            z-index: 1;
+            background-color: #212121
+        }
+
+        .btn2:hover {
+            transform: scale(1.1)
+        }.btn2:active {
+            border: double 4px #4b9b9b;
+            background-origin: border-box;
+            background-clip: content-box, border-box;
+            animation: none
+        }
+
+        .btn2:active .circle {
+            background: #e600f1
+        }
+
+        #stars {
+            position: relative;
+            background: 0 0;
+            width: 200rem;
+            height: 200rem
+        }
+
+        #stars::after {
+            content: "";
+            position: absolute;
+            top: -10rem;
+            left: -100rem;
+            width: 100%;
+            height: 100%;
+            animation: animStarRotate 90s linear infinite
+        }
+
+        #stars::after {
+            background-image: radial-gradient(#fff 1px, transparent 1%);
+            background-size: 50px 50px
+        }
+
+        #stars::before {
+            content: "";
+            position: absolute;
+            top: 0;
+            left: -50%;
+            width: 170%;
+            height: 500%;
+            animation: animStar 60s linear infinite
+        }
+
+        #stars::before {
+            background-image: radial-gradient(#fff 1px, transparent 1%);
+            background-size: 50px 50px;
+            opacity: .5
+        }
+
+        @keyframes animStar {
+            from {
+                transform: translateY(0)
+            }
+
+            to {
+                transform: translateY(-135rem)
+            }
+        }
+
+        @keyframes animStarRotate {
+            from {
+                transform: rotate(360deg)
+            }
+
+            to {
+                transform: rotate(0)
+            }
+        }
+
+        @keyframes gradient_301 {
+            0% {
+                background-position: 0 50%
+            }
+
+            50% {
+                background-position: 100% 50%
+            }
+
+            100% {
+                background-position: 0 50%
+            }
+        }
+
+        @keyframes pulse_3011 {
+            0% {
+                transform: scale(.75);
+                box-shadow: 0 0 0 0 rgba(0, 0, 0, .7)
+            }
+
+            70% {
+                transform: scale(1);
+                box-shadow: 0 0 0 10px transparent
+            }
+
+            100% {
+                transform: scale(.75);
+                box-shadow: 0 0 0 0 transparent
+            }
+        }
+
+        .artikel {
+            text-align: justify;
+        }
+
+        .artikel p {
+            margin: 25px 0;
+        }
+
+        .artikel a {
+            color: #e7d10c
+        }
+        
+        .artikel h1 {
+            color: #e7d10c;
+            text-align: center;
+        }
+
+        .artikel h2 {
+            color: #e7d10c;
+            text-align: center;
+        }
+
+        .block-img {
+            position: relative;
+            margin: auto;
+            min-width: 100px;
+            background: linear-gradient(0deg, #02012b, #e600f1);
+            color: #fff;
+            cursor: pointer;
+            margin-bottom: 20px;
+        }
+
+        .block-img:before,
+        .block-img:after {
+            content: '';
+            position: absolute;
+            right: -3px;
+            bottom: -3px;
+            background: linear-gradient(45deg, #e2b033, #f3c23c, #8d5033, #000000, #000622);
+            background-size: 400%;
+            width: calc(100% + 6px);
+            height: calc(100% + 6px);
+            z-index: -1;
+            animation: steam 5s linear infinite;
+            border-radius: 10px;
+        }
+
+        .block1 {
+            position: relative;
+            margin: auto;
+            min-width: 100px;
+            padding: 5px 10px;
+            background: linear-gradient(0deg, #02012b, #e600f1);
+            color: #fff;
+            cursor: pointer;
+        }
+
+        .block1:before,
+        .block1:after {
+            content: '';
+            position: absolute;
+            right: -3px;
+            bottom: -3px;
+            background: linear-gradient(45deg, #e2b033, #f3c23c, #8d5033, #000000, #000622);
+            background-size: 400%;
+            width: calc(100% + 6px);
+            height: calc(100% + 6px);
+            z-index: -1;
+            animation: steam 5s linear infinite;
+            border-radius: 10px;
+        }
+
+        .block {
+            position: relative;
+            padding: 8px 10px;
+            background: linear-gradient(0deg, #02012b, #e600f1);
+            color: #fff;
+            cursor: pointer;
+            margin-bottom: 20px;
+        }
+
+        .block:before,
+        .block:after {
+            content: '';
+            position: absolute;
+            right: -3px;
+            bottom: -3px;
+            background: linear-gradient(45deg, #e2b033, #f3c23c, #8d5033, #000000, #000622);
+            background-size: 400%;
+            width: calc(100% + 6px);
+            height: calc(100% + 6px);
+            z-index: -1;
+            animation: steam 5s linear infinite;
+        }
+
+        @keyframes steam {
+            0% {
+                background-position: 0 0;
+            }
+
+            50% {
+                background-position: 400% 0;
+            }
+
+            100% {
+                background-position: 0 0;
+            }
+        }
+
+        .block:after {
+            filter: blur(50px);
+        }
+
+        .footer {
+            text-align: center;
+            margin: 15px 0;
+        }
+
+        .footer a {
+            color: #e7d10c
+        }
+    </style>
+</head>
+
+<body>
+  <main>
+    <div class="Falisha-toto">
+      <div class="toto-SLOT">
+        <div class="Shankara-container block1">
+          <a href="https://wa.me/85567994966">
+            <button class="WinaSlot"><img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjHq2qgkbYu28PorookomKRMJ3hULVsriCA6jfXcpIOhF5vIbf6Rl5Mh5CK5PrCnK2-yxiuVvPbPBQ-bPZKnXMZyF6ODOI1PUsSniBpUlRu6TeP2OlQkKoJBfCYHQOLgaTxawb8HDywwfIR4ZH5mz5i20pYGFhLNLHln3wAa-4xJSsL7l3Lzs2AK0C_h3KP/s256/3.png" width="20px"></button>
+          </a>
+          <a href="https://wa.me/85567994966">
+            <button class="WinaSlot"><img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiYYk2oFGHUfZUaH-t8QBwh9eCnIklSmRoKA8hay3vEgvQ5f94UsWj1ZCci9n_Pp_pI3XkKtxkT9nVngRvJlb8u9WO7QmvUaquG-3ufljiQvhZkrQk0qGOkZX6KNlnt8gniA7aApb-6-X0cj0Uu5nch7ZWnVVHBWzC6_yiDRbIJdc6ewFHtZ7WrspVfuHqG/s50/whatsapp_w2bfkm.png" width="20px"></button>
+          </a>
+          <a href="https://wa.me/85567994966">
+            <button class="WinaSlot"><img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhHkBIc3uyG1B8dhWwaGbS1m5nUbOxltBU_kztfX4lqnuOsDqR6I69TKcviQePoXX7PKzZ5SbYkT9SjzmurUJissZfMjWOYl0J98wC1NU4w4Su8krmmDSF-OpR4aEz1fRBnn9Rc3_wQc-wd98L0-fVogB93VO8X_bykp41lb2Cxaro1CjwnK81Bx70f-sMc/s101/counter-icon-1.png" width="20px"></button>
+          </a>
+          <a href="https://wa.me/85567994966">
+            <button class="WinaSlot"><img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiOPYtrTZ7zLzGyRoxSFvFvMm0mh73LUh2DrsOUTEn2SyDpRaFiG5yPwRLayz3PRTGjL8Q-kxrfso7NeTQyF0SczxQNqLgiha2QID8qrG3iUsvV8XCUPFJwq0AOxqL_3QuaknRy1lvtOF0vbRkxwH929np_gF4UAmMGyKGvugOx1dchlqWHX81MqJJNqeyr/s89/line.png" width="20px"></button>
+          </a>
+        </div>
+        <br>
+
+        <div class="block-img">
+          <a href="https://wa.me/85567994966" target="_blank" rel="noopener noreferrer nofollow">
+            <amp-img height="780" width="1200" layout="responsive" alt="Toto Togel" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj-uQNa2ncdwAg_lw6BpN7DrotZR__Hms3B-ZdoT5R8ZZZ0O0S_sMPhskgHWGb0poqdLKRDA3ov1yC88nEpi0KXaMBqRLLPtm-t5XOXBEic5F2nyn0nq45ev2NXv2nSt4DdwW9jOORJUpP2KGGrPy1TrGgebcdFxaTNoxB9qiJJeBcwzXeHbOh_w1R5l6rF/s800/SHANKARA%20-%20BONUS%20NEW%20MEMBER%20100%25.jpg"></amp-img>
+          </a>
+        </div>
+
+        <div class="Shankara">
+          <a href="https://wa.me/85567994966 " target="_blank" rel="noopener noreferrer nofollow">
+            <button class="btn2"><strong>DAFTAR AKUN VIP</strong>
+              <div id="container-stars">
+                <div id="stars"></div>
+              </div>
+              <div id="glow">
+                <div class="circle"></div>
+                <div class="circle"></div>
+              </div>
+            </button>
+          </a>
+          <a href="https://wa.me/85567994966" target="_blank" rel="noopener noreferrer nofollow" class="block">LOGIN AKUN</a>
+          <a href="https://wa.me/85567994966" target="_blank" rel="noopener noreferrer nofollow" class="block">DAFTAR AKUN</a>
+          <a href="https://wa.me/85567994966" target="_blank" rel="noopener noreferrer nofollow" class="block">VPN UNTUK SLOT ANTI BLOK</a>
+        </div>
+      </div>
+      <div class="artikel">
+
+        <h1>Wina Slot ‍🔥 Daftar Situs Toto Togel Terpercaya Di Indonesia</h1>
+        <p>Situs Toto Togel Terpercaya merupakan platform yang menawarkan pengalaman bermain togel yang unik dan menarik. Keunggulan utama dari situs ini adalah keamanan dan kepercayaan yang tinggi. Dengan menggunakan teknologi terbaru, situs ini menjaga privasi dan kerahasiaan data para pemain. Selain itu, situs juga menyediakan berbagai macam pasaran togel dengan odds yang kompetitif, sehingga memberikan peluang besar untuk memenangkan hadiah yang menggiurkan.</p>
+
+        <p>Proses transaksi yang cepat dan mudah juga menjadi salah satu keunggulan dari situs ini. Dengan layanan pelanggan yang ramah dan responsif, pemain dapat merasa nyaman dan puas dalam bermain togel di situs ini. Dalam rangka meningkatkan pengalaman bermain, situs ini juga menyediakan fitur-fitur seperti live chat dan prediksi togel yang akurat.</p>
+      </div>
+    </div>
+    <hr>
+    <div class="footer">
+      &copy; 2014 - 2024 | <a href="https://elearningportal.trinita.ac.id/dataWINS374/">Toto Togel</a>
+    </div>
+  </main>
+</body>
+</html>
